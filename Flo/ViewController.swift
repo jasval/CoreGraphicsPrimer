7c12928f6d05e82c8936c9d4081f164fc10f5d51
Jasper <j.valdivia@returngenerics.com>
2021-08-22 11:27:25 +0100

    Adding Arcs and working with radians

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -5,15 +5,18 @@ import UIKit
 class ViewController: UIViewController {
     
     var addButton: PushButton!
+    var counterView: CounterView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         drawButton()
+        drawCounter()
     }
     
     func drawButton() {
         addButton = PushButton()
+        addButton.backgroundColor = .clear
         addButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(addButton)
         
@@ -22,6 +25,21 @@ class ViewController: UIViewController {
             addButton.widthAnchor.constraint(equalToConstant: 50),
             addButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             addButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func drawCounter() {
+        counterView = CounterView()
+        counterView.backgroundColor = .clear
+        counterView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(counterView)
+        
+        
+        NSLayoutConstraint.activate([
+            counterView.heightAnchor.constraint(equalToConstant: 230),
+            counterView.widthAnchor.constraint(equalToConstant: 230),
+            counterView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            counterView.bottomAnchor.constraint(equalTo: addButton.topAnchor, constant: 40)
         ])
     }
     
